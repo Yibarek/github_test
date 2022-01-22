50342dcf5cc219df4a2661fdb1154451c5572051
Yitbarek <ytuman833@gmail.com>
2022-01-21 22:18:12 -0800

    first

---
+++ namer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"Hey budy what is your name?\n";
+    cin>>name;
+    cout<<"Hello "<<name<<" welcome to git!\n";
+    return 0;
+}
